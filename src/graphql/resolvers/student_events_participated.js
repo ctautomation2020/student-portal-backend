bacf5f386d83a2e282bb93355a018935c0b06738
surya71 <surya7102000@gmail.com>
2020-12-24 00:34:05 +0530

    Events_participated

---
+++ src/graphql/resolvers/student_events_participated.js
@@ -0,0 +1,98 @@
+import getRegNo from "../auth/getRegNo"
+let fs = require('fs')
+let path = require('path')
+
+module.exports={
+    Query:{
+        async studentEventsParticipated(parent, {data}, {prisma,req}, info){
+            const Register_No = data.Register_No;
+            return await prisma.student_events_participated.findMany({
+                where: {
+                    Register_No
+                }
+            })
+        },
+        
+        async eventParticipated(parent,{data}, {prisma,req}, info){
+            const Event_ID = data.Event_ID
+            return await prisma.student_events_participated.findOne({
+                where: {
+                    Event_ID
+                }
+            })
+        }
+    },
+
+    Mutation: {
+        
+        async createEventParticipated(parent, {data}, {prisma,req}, info) {
+            const {Event_Type_Ref,Participation_Type_Ref,...ref_data} = data;
+            const Register_No = getRegNo(req);
+
+            if(Event_Type_Ref){
+                ref_data.person_reference_table_person_reference_tableTostudent_Event_Type_Ref={
+                    connect:{
+                        Reference_ID:Event_Type_Ref
+                    }
+                }
+            }
+
+            if(Participation_Type_Ref){
+                ref_data.person_reference_table_person_reference_tableTostudent_Participation_Type_Ref={
+                    connect:{
+                        Reference_ID:Participation_Type_Ref
+                    }
+                }
+            }
+
+            return await prisma.student_events_participated.create({
+                data:{
+                    student:{
+                        connect:{
+                            Register_No:Register_No
+                        }
+                    },
+                    ...ref_data
+                }
+            })
+        },
+
+        async updateEventParticipated(parent, {data}, {prisma}, info) {
+            const {Event_ID,Event_Type_Ref,Participation_Type_Ref,...ref_data} = data;
+
+            if(Event_Type_Ref){
+                ref_data.person_reference_table_person_reference_tableTostudent_Event_Type_Ref={
+                    connect:{
+                        Reference_ID:Event_Type_Ref
+                    }
+                }
+            }
+
+            if(Participation_Type_Ref){
+                ref_data.person_reference_table_person_reference_tableTostudent_Participation_Type_Ref={
+                    connect:{
+                        Reference_ID:Participation_Type_Ref
+                    }
+                }
+            }
+            return await prisma.student_events_participated.update({
+                where:{
+                    Event_ID
+                },
+                data:{
+                    ...ref_data
+                }
+            })
+        },
+
+        async deleteEventParticipated(parent, {data}, {prisma}, info) {
+            
+            return await prisma.student_events_participated.delete({
+                where:{
+                    Event_ID: data.Event_ID
+                }
+            })
+        }
+    }
+    
+}
