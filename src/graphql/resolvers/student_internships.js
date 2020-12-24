7582c8cbf9e9a46cf871714bc13b4c8265e1fc9f
surya71 <surya7102000@gmail.com>
2020-12-25 00:53:39 +0530

    student-internship

---
+++ src/graphql/resolvers/student_internships.js
@@ -0,0 +1,98 @@
+import getRegNo from "../auth/getRegNo"
+let fs = require('fs')
+let path = require('path')
+
+module.exports={
+    Query:{
+        async studentInternships(parent, {data}, {prisma,req}, info){
+            const Register_No = data.Register_No;
+            return await prisma.student_internship.findMany({
+                where: {
+                    Register_No
+                }
+            })
+        },
+        
+        async studentInternship(parent,{data}, {prisma,req}, info){
+            const Internship_ID = data.Internship_ID
+            return await prisma.student_internship.findOne({
+                where: {
+                    Internship_ID
+                }
+            })
+        }
+    },
+
+    Mutation: {
+        
+        async createStudentInternship(parent, {data}, {prisma,req}, info) {
+            const {Stiphend_Option_Ref,Selection_Mode_Ref,...ref_data} = data;
+            const Register_No = getRegNo(req);
+
+            if(Selection_Mode_Ref){
+                ref_data.person_reference_table_person_reference_tableTostudent_internship_Selection_Mode_Ref={
+                    connect:{
+                        Reference_ID:Selection_Mode_Ref
+                    }
+                }
+            }
+
+            if(Stiphend_Option_Ref){
+                ref_data.person_reference_table_person_reference_tableTostudent_internship_Stiphend_Option_Ref={
+                    connect:{
+                        Reference_ID:Stiphend_Option_Ref
+                    }
+                }
+            }
+
+            return await prisma.student_internship.create({
+                data:{
+                    student:{
+                        connect:{
+                            Register_No:Register_No
+                        }
+                    },
+                    ...ref_data
+                }
+            })
+        },
+
+        async updateStudentInternship(parent, {data}, {prisma}, info) {
+            const {Internship_ID,Stiphend_Option_Ref,Selection_Mode_Ref,...ref_data} = data;
+            
+            if(Selection_Mode_Ref){
+                ref_data.person_reference_table_person_reference_tableTostudent_internship_Selection_Mode_Ref={
+                    connect:{
+                        Reference_ID:Selection_Mode_Ref
+                    }
+                }
+            }
+
+            if(Stiphend_Option_Ref){
+                ref_data.person_reference_table_person_reference_tableTostudent_internship_Stiphend_Option_Ref={
+                    connect:{
+                        Reference_ID:Stiphend_Option_Ref
+                    }
+                }
+            }
+            return await prisma.student_internship.update({
+                where:{
+                    Internship_ID
+                },
+                data:{
+                    ...ref_data
+                }
+            })
+        },
+
+        async deleteStudentInternship(parent, {data}, {prisma}, info) {
+            
+            return await prisma.student_internship.delete({
+                where:{
+                    Internship_ID: data.Internship_ID
+                }
+            })
+        }
+    }
+    
+}
