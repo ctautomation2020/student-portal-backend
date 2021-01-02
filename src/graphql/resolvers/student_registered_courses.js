aa814104a9e90e3b9aed6bac30a39388ce5e36a1
surya71 <surya7102000@gmail.com>
2021-01-02 18:06:28 +0530

    student_registered_courses

---
+++ src/graphql/resolvers/student_registered_courses.js
@@ -0,0 +1,18 @@
+const { TransformQuery } = require("graphql-tools");
+
+module.exports={
+    Query:{
+        async studentRegisteredCourses(parent,{data}, {prisma,req}, info){
+            const reg_no = data.Register_No
+            return  await prisma.course_registered_students.findMany({
+                include: {
+                    course_list: true
+                },
+                where:{
+                    reg_no
+                }
+            })
+        }
+
+    },
+}
