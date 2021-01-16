36ef371af5a93e1419250c26ea6e2bf20fef6ee8
sivaganesh193 <sivaganeshb193@gmail.com>
2021-01-16 10:51:35 +0530

    assessment and db

---
+++ src/graphql/resolvers/course_assesment.js
@@ -0,0 +1,72 @@
+module.exports = {
+    Query: {
+        async assessment(parent, {data}, {prisma}, info){
+            return await prisma.course_assessment.findMany({
+                where:{
+                    ...data
+                },
+                orderBy:{
+                    question_num:'asc'
+                }
+            })
+        },
+        async session_assessments(parent, {data}, {prisma}, info){
+            const a =  await prisma.course_assessment.findMany({
+                where:{
+                    ...data
+                },
+                orderBy:{
+                    assess_num:'asc'
+                },
+                select: {
+                    assess_num: true
+                },
+                distinct: ["assess_num"]
+            })
+            const assess = []
+            a.forEach((a)=>{
+                assess.push(a.assess_num)
+            })
+
+            return assess
+        }
+
+    },
+
+    Mutation: {
+        async createAssessment(parent, {data}, {prisma,auth,req}, info){
+            //console.log(JSON.stringify(data.section[0].questions[0],null,2))
+            const Person_ID = auth(req)
+            const {course_code,group_ref,session_ref,assess_num,entry_date, ...remData} = data
+            remData.section.forEach(async(sec)=>{
+                sec.questions.forEach(async(ques)=>{
+                    await prisma.course_assessment.create({
+                        data:{
+                            course_list:{
+                                connect: {
+                                    course_code
+                                }
+                            },
+                            course_reference_table_course_assessment_group_refTocourse_reference_table:{
+                                connect: {
+                                    reference_id: group_ref
+                                }
+                            },
+                            course_reference_table_course_assessment_session_refTocourse_reference_table:{
+                                connect: {
+                                    reference_id: session_ref
+                                }
+                            },
+                            assess_num,
+                            entry_date,
+                            section: sec.name,
+                            ...ques
+
+                        }
+                    })
+                })
+            })
+
+        }
+    }
+}
---
+++ src/graphql/resolvers/course_assesseval.js
@@ -0,0 +1,72 @@
+module.exports = {
+    Query: {
+        async assess_evaluation(parent, {data}, {prisma}, info) {
+            return await prisma.course_assesseval.findMany({
+                where:{
+                    ...data
+                },
+                orderBy:{
+                    question_num:'asc'
+                }
+            })
+        }
+    },
+
+    Mutation: {
+        async createAssess_evaluation(parent, {data}, {prisma,auth,req}, info) {
+            const Person_ID = auth(req)
+            const {course_code,group_ref,session_ref,assess_num,reg_no, ...remData} = data
+            
+            remData.questions.forEach(async(ques)=>{
+                await prisma.course_assesseval.create({
+                    data:{
+                        course_list:{
+                            connect: {
+                                course_code
+                            }
+                        },
+                        student_list:{
+                            connect: {
+                                reg_no
+                            }
+                        },
+                        course_reference_table_course_assesseval_group_refTocourse_reference_table:{
+                            connect: {
+                                reference_id: group_ref
+                            }
+                        },
+                        course_reference_table_course_assesseval_session_refTocourse_reference_table:{
+                            connect: {
+                                reference_id: session_ref
+                            }
+                        },
+                        assess_num,
+                        ...ques
+                    }
+                })
+            })
+
+        },
+
+        async updateAssess_evaluation(parent, {data}, {prisma,auth,req}, info) {
+            const Person_ID = auth(req)
+            const {course_code,group_ref,session_ref,assess_num,reg_no, ...remData} = data
+            
+            remData.questions.forEach(async(ques)=>{
+                await prisma.course_assesseval.updateMany({
+                    where:{
+                        course_code,
+                        group_ref,
+                        session_ref,
+                        assess_num,
+                        reg_no,
+                        question_num: ques.question_num
+                    },
+                    data:{
+                        mark:ques.mark
+                    }
+                })
+            })
+        }
+    }
+}
